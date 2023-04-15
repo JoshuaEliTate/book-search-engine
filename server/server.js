237019c1bc2516bbdda89bf261c306c99b5502f3
Josh <joshuaelitate@gmail.com>
2023-04-14 21:39:27 -0700

    final touches

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ app.use( "/api", routes);
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
